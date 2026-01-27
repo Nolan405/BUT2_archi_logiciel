79c3cfed284b88257f18de3f7b9a9aa97c0a38f2
Nolan405 <nolan.morain@etu.univ-orleans.fr>
2026-01-27 16:59:41 +0100

    avancemment tp2

--- quiz/models.py
+++ quiz/models.py
@@ -6,6 +6,7 @@ class Questionnaire:
     def __init__(self, name):
         self.name = name
         self.id = Questionnaire.cpt_id
+        self.questions = []
         Questionnaire.cpt_id += 1
         Questionnaire.questionnaires.append(self)
 
@@ -14,6 +15,12 @@ class Questionnaire:
     
     def get_nom(self):
         return self.name
+    
+    def set_nom(self, name):
+        self.name = name
+
+    def add_question(self, question):
+        self.questions.append(question)
     
     @staticmethod
     def get_questionnaires():
@@ -27,28 +34,80 @@ class Questionnaire:
 
     @staticmethod
     def create_questionnaire(nom):
-        Questionnaire(nom)
+        return Questionnaire(nom)
+    
+    @staticmethod
+    def update_questionnaire(id, nom):
+        for q in Questionnaire.questionnaires:
+            if q.get_id() == id:
+                q.set_nom(nom)
+                return q
+        return None
 
     @staticmethod
     def delete_questionnaire(id):
         for q in Questionnaire.questionnaires:
             if q.get_id() == id:
                 Questionnaire.questionnaires.remove(q)
+                return True
+        return False
 
     def questionnaire_to_json(self):
+        questions_json = []
+        for question in self.questions:
+            questions_json.append(question.question_to_json())
+
         json = {
             "id": self.get_id(),
-            "nom": self.get_nom()
+            "nom": self.get_nom(),
+            "questions": questions_json
         }
         return json
 
 
-themes = [
-    "Culture Générale", "Géographie", "Histoire", "Sciences", 
-    "Cinéma", "Sport", "Musique", "Littérature", 
-    "Technologie", "Gastronomie"
-]
+class Question:
 
-for theme in themes:
-    Questionnaire.create_questionnaire(theme)
+    def __init__(self, numero, enonce):
+        self.numero = numero
+        self.enonce = enonce
 
+    def get_numero(self):
+        return self.numero
+    
+    def get_enonce(self):
+        return self.enonce
+    
+    def question_to_json(self):
+        json = {
+            "numero": self.get_numero(),
+            "enonce": self.get_enonce()
+        }
+        return json
+    
+
+
+
+questions_data = {
+    "Culture Générale": [
+        "Quelle est la capitale de la France ?",
+        "Qui a peint la Joconde ?",
+        "Quel est le plus grand mammifère marin ?"
+    ],
+    "Géographie": [
+        "Quel est le plus long fleuve du monde ?",
+        "Sur quel continent se trouve le mont Kilimandjaro ?",
+        "Quelle est la capitale du Japon ?"
+    ],
+    "Histoire": [
+        "En quelle année a commencé la Révolution française ?",
+        "Qui était le premier président des États-Unis ?",
+        "Quelle est la date de la chute du mur de Berlin ?"
+    ]
+}
+
+for theme, liste_enonces in questions_data.items():
+    questionnaire = Questionnaire.create_questionnaire(theme)
+    
+    for i, enonce in enumerate(liste_enonces, 1):
+        nouvelle_question = Question(i, enonce)
+        questionnaire.add_question(nouvelle_question)
--- quiz/views.py
+++ quiz/views.py
@@ -7,7 +7,53 @@ from .models import Questionnaire
 @app.route('/quiz/api/v1.0/questionnaires', methods=['GET'])
 def get_questionnaires():
     public_questionnaires = []
-    for questionnaire in Questionnaire.get_questionnaires():
-        public_questionnaires.append(questionnaire)
+    all_questionnaires = Questionnaire.get_questionnaires()
+    
+    for questionnaire in all_questionnaires:
+        public_questionnaires.append(questionnaire.questionnaire_to_json())
         
     return jsonify({'questionnaires': public_questionnaires})
+
+
+
+@app.route('/quiz/api/v1.0/questionnaires/<int:questionnaire_id>', methods=['GET'])
+def get_questionnaire(questionnaire_id):
+    questionnaire = Questionnaire.get_questionnaire(questionnaire_id)
+    if questionnaire is None:
+        return abort(404)
+    
+    questionnaire.questionnaire_to_json()
+    return jsonify({'questionnaires': questionnaire.questionnaire_to_json()})
+
+
+
+@app.route('/quiz/api/v1.0/questionnaires', methods=['POST'])
+def create_questionnaire():
+    if not request.json or not 'nom' in request.json:
+        return abort(400)
+    
+    nom_questionnaire = request.json['nom']
+    nouvelle_quest = Questionnaire.create_questionnaire(nom_questionnaire)
+    return jsonify({'result': nouvelle_quest.questionnaire_to_json()}), 201
+
+
+
+@app.route('/quiz/api/v1.0/questionnaires/<int:questionnaire_id>', methods=['PUT'])
+def update_questionnaire(questionnaire_id):
+    if not request.json or not 'nom' in request.json:
+        return abort(400)
+    
+    new_nom = request.json['nom']
+    update_quest = Questionnaire.update_questionnaire(questionnaire_id, new_nom)
+    if update_quest is None:
+        return abort(404)
+    return jsonify({'result': update_quest.questionnaire_to_json()}), 201
+
+
+
+@app.route('/quiz/api/v1.0/questionnaires/<int:questionnaire_id>', methods=['DELETE'])
+def delete_questionnaire(questionnaire_id):
+    boolean = Questionnaire.delete_questionnaire(questionnaire_id)
+    if not boolean:
+        return abort(404)
+    return jsonify({"status": "deleted"})
