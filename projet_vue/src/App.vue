bf45f7bede5001834c3a26ca593aac2ded9b3b5d
Nolan405 <nolan.morain@etu.univ-orleans.fr>
2026-03-11 10:29:55 +0100

    avancemment projet vueJs

--- projet_vue/src/App.vue
+++ projet_vue/src/App.vue
@@ -1,11 +1,12 @@
 <script>
 
-import TodoItem from ’./components/TodoItem.vue’;
+import TodoItem from './components/TodoItem.vue';
 
 let data = {
   todos: [{ text: 'Faire les courses', checked: true }, { text: 'Apprendre REST', checked: false }],
   title: 'Mes tâches',
-  newItem: ''
+  newItem: '',
+  currentIndex: -1
 };
 
 export default {
@@ -22,9 +23,18 @@ export default {
           checked: false
         });
         this.newItem = '';
-      } 
+      }
+    },
+    suppItem: function (index) {
+      this.todos.splice(index, 1);
+    },
+    modifItem: function (index) {
+      this.currentIndex = index;
+    },
+    fermerModif: function () {
+      this.currentIndex = -1;
     }
-  }
+  },
   components: { TodoItem }
 }
 </script>
@@ -34,15 +44,22 @@ export default {
    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
  <div  class="container">
 <h2>{{ title }}</h2>
+  <div v-if="currentIndex !== -1" class="alert">
+    <input 
+      type="text" 
+      class="form-control" 
+      v-model="todos[currentIndex].text"
+      @keyup.enter="fermerModif"
+    >
+  </div>
   <ol>
-    <li v-for= "todo in todos" 
-        v-bind:class="{ 'alert alert-success': todo.checked }">
-      <div class="checkbox">
-        <label>
-          <input type="checkbox" v-model="todo.checked"> {{ todo.text }}
-        </label>
-      </div>
-    </li>
+    <todo-item 
+        v-for="(todo, index) in todos" 
+        :key="index" 
+        :todo="todo"
+        @remove="suppItem(index)"
+        @modify="modifItem(index)"
+      />
   </ol>
   <div class="input-group">
     <input v-model="newItem" 
@@ -58,4 +75,3 @@ export default {
   </div>
 </div>
 </template>
-
